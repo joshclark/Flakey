4c1ad7a19d8f987d5ead67a883f001d02272f582
Josh Clark <josh@goclarks.com>
2016-05-16 15:36:09 -0500

    Adding a stress test project to run tests longer than traditional unit tests.

---
+++ test/Flakey.StressTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flakey.StressTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cpuCount = Environment.ProcessorCount;
+            Console.WriteLine($"Running stress tests for {cpuCount} processors...");
+
+            int iterationsPerProc = 20 * 1000000;
+            var gen = new IdGenerator(1);
+
+            Console.WriteLine($"Generating {iterationsPerProc} ids per processor.");
+
+            var allTasks = new List<Task<LinkedList<long>>>();
+            foreach (int cpu in Enumerable.Range(0, cpuCount))
+            {
+                allTasks.Add(GetIds(gen, iterationsPerProc));
+            }
+
+            Task.WhenAll(allTasks).Wait();
+
+            Console.WriteLine($"Verifying generated ids...");
+
+            foreach (var task in allTasks)
+            {
+                VerifyIds(task.Result);
+            }
+
+            Console.WriteLine("All done.");
+        }
+
+        private static Task<LinkedList<long>> GetIds(IdGenerator gen, int count)
+        {
+            return Task.Run(() => new LinkedList<long>(gen.Take(count)));
+        }
+
+        private static void VerifyIds(LinkedList<long> result)
+        {
+            int index = 0;
+            var current = result.First.Next;
+            while (current != null)
+            {
+                if (current.Previous != null) 
+                {
+                    if (current.Value <= current.Previous.Value)
+                    {
+                        Console.WriteLine($"Results out of order: Result[{index - 1}]: {current.Previous.Value}  Result[{index}]: {current.Value}");
+                    }
+                }
+                current = current.Next;
+                index++;
+            }
+        }
+
+
+    }
+}
---
+++ test/Flakey.StressTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Flakey.StressTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Flakey.StressTest")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58fcee0e-94a3-4c50-a2f2-aa1829c3eac8")]
